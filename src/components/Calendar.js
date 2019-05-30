a994a92f9be4daa32788b80d8ccadb790cee9842
Nocktiss <anthony.grupposo@gmail.com>
2019-05-30 03:27:09 +0200

    add link on Calendar.js

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,9 +1,15 @@
 import React, {Component} from 'react'
 
+import {Link} from 'react-router-dom';
+
 class Calendar extends Component{
     render(){
         return(
-            <h1>CALENDRIER</h1>
+            <div>
+            <Link to="/select-form" ><input type="submit" value="Admin" /></Link>
+            <Link to="/login" ><input type="submit" value="Login" /></Link>
+            </div>
+            
         )
     }
 }
