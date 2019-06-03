8fcc463f04e3bafe3b1aeca9980aa7f5aeff7037
QuietMonkey <taillandierbertrand@gmail.com>
2019-06-03 16:30:53 +0200

    add one week in the fake json then create logic for arrows

--- src/components/Weekly.jsx
+++ src/components/Weekly.jsx
@@ -1,21 +1,41 @@
-import React from 'react'
+import React, {Component} from 'react'
+import Week from './Week'
 import Day from './Day'
 import dataTest from "./dataTest.json"
 
-const Weekly = () => {
-    const week = dataTest.map((day) => <Day date={day.date} dataEvent={day.data} />)
-    return (
-        <div className='weekly'>
-            <div className='weeklyHead'>
-                <h1>Agenda Mars 2019</h1>
+class Weekly extends Component {
+    state= {
+        days:[]
+    }
+
+    componentWillMount(){
+        this.setState({days: dataTest.filter((display)=> display.id <5)})
+    }
+
+    previousDays = () => {
+        this.setState({days: dataTest.filter((display)=> display.id <5)})
+    }
+
+    nextDays = () => {
+        this.setState({days: dataTest.filter((display)=> display.id >4)})
+    }
+
+    week = this.state.days.map((day) => <Day date={day.date} dataEvent={day.data} />)
+    
+    render() {
+        return (
+            <div className='weekly'>
+                <div className='weeklyHead'>
+                    <h1>Agenda Mars 2019</h1>
+                </div>
+                <div className='weeklyDisplay'>
+                    <div onClick={this.previousDays}><i class="arrow left"></i></div>
+                    <Week dataDays={this.state.days} />
+                    <div onClick={this.nextDays}><i class="arrow right"></i></div>
+                </div>
             </div>
-            <div className='weeklyDisplay'>
-                <div><i class="arrow left"></i></div>
-                {week}
-                <div><i class="arrow right"></i></div>
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Weekly
