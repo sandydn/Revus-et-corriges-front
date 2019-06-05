a938c7a8a5ee2e5cca319188405b028c4ba992c3
Nocktiss <anthony.grupposo@gmail.com>
2019-06-05 16:51:33 +0200

    add login.js / login.css and 2 pictures

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
-import { Route, BrowserRouter, Switch, NavLink } from 'react-router-dom'
 import Calendar from './components/Calendar'
 import Menu from './components/Menu'
-import Logo from './components/Logo';
 
 import './components/Logo.css';
 import './App.css'
 import Sign from './screen/Sign';
 import { Route, BrowserRouter, Switch } from 'react-router-dom'
-import Calendar from './components/Calendar'
 import SelectionForm from './screen/SelectionForm';
 import FormCinema from './screen/FormCinema';
 
@@ -20,7 +17,6 @@ class App extends Component {
           <img src="https://trello-attachments.s3.amazonaws.com/5ce27d2b766e4e0b604c85c9/936x500/0d501f2b8aed46802100818f7a936f74/rc_logo_final-021-e1521992230333.png"/>
           <Menu />
           {/* <NavLink exact to="/" activeClassName="selected"> Calendrier </NavLink> */}
-          <Logo />
           <Switch>
             <Route exact path="/" component={Calendar} />
             <Route path="/login" component={Sign} />
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,65 +1,107 @@
 import React from 'react'
 import axios from 'axios'
-import icon from '../pictures/eye.png'
-import {Link} from 'react-router-dom';
-// import SelectionForm from './SelectionForm'
+import { Link } from 'react-router-dom';
 
-// import '../App.css'
+import TextField from '@material-ui/core/TextField';
+import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Button from '@material-ui/core/Button';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+
+import iconLogin from '../pictures/login.png'
+import User from '../pictures/eugenie.png'
+import './Login.css'
 
 class Login extends React.Component {
-    state = {
-        email: "",
-        password: "",
-        showPassword: false,
-        success: false
-    }
+	state = {
+		email: "",
+		password: "",
+		showPassword: false,
+		success: false
+	}
 
-    toggleShow = event => {
-        this.setState({ showPassword: !this.state.showPassword })
-    }
+	toggleShow = event => {
+		this.setState({ showPassword: !this.state.showPassword })
+	}
 
-    handleChange = event => {
-        this.setState({ [event.target.name]: event.target.value })
-    }
+	handleChange = event => {
+		this.setState({ [event.target.name]: event.target.value })
+	}
 
-    handleSubmit = event => {
-        event.preventDefault()
-        axios.post(`http://localhost:4242/adminform`,{
-            email: event.target.email.value,
-            password: event.target.password.value
-        })
-        .then(() => {
-            this.setState({ success: true })
-        })
-    }
-// ON SUBMIT - rediriger avec le routeur sur la page d'appel de formulaire
+	handleSubmit = event => {
+		event.preventDefault()
+		axios.post(`http://localhost:4242/adminform`, {
+			email: event.target.email.value,
+			password: event.target.password.value
+		})
+			.then(() => {
+				this.setState({ success: true })
+			})
+	}
+	// ON SUBMIT - rediriger avec le routeur sur la page d'appel de formulaire
 
-    render() {
-        return (
-            <div className="adminForm">
-                <form onSubmit={this.handleSubmit}>
-                    <input
-                    type="text" 
-                    name="email" 
-                    placeholder="@"
-                    value={this.state.email}
-                    onChange={this.handleChange}/>
+	render() {
+		return (
+			<div>
 
-                    <input 
-                    type= {this.state.showPassword ? "password" : "text"}
-                    name="password" 
-                    placeholder="Password"
-                    value={this.state.password}
-                    onChange={this.handleChange}/>
-                    <img src={icon} className="iceye" onClick={this.toggleShow} alt="eye"/>
+				<form className="adminForm" onSubmit={this.handleSubmit}>
+					<img className="iconLogin" src={iconLogin} alt="icone-login" />
+					<img className="iconUser" src={User} alt="icone-user" />
 
-                    <Link to="/select-form" ><input type="submit" value="Submit" /></Link>
-                </form>
-                {this.state.success ? <p>Vous êtes connecté.</p> : null}
-            </div>
-        )
-    }
+					<TextField
+						margin="normal"
+						fullWidth
+						id="email"
+						label="Email"
+						name="email"
+						autoComplete="email"
+						autoFocus
+					/>
+
+					<FormControl className="">
+						<InputLabel htmlFor="adornment-password">Password</InputLabel>
+						<Input
+							id="adornment-password"
+							name="password"
+							fullWidth
+							type={this.state.showPassword ? 'text' : 'password'}
+							value={this.state.password}
+							onChange={this.handleChange}
+							endAdornment={
+								<InputAdornment position="end">
+									<IconButton aria-label="Toggle password visibility" onClick={this.toggleShow}>
+										{this.state.showPassword ? <Visibility /> : <VisibilityOff />}
+									</IconButton>
+								</InputAdornment>} />
+					</FormControl>
+
+					<FormControlLabel
+						control={<Checkbox value="remember" color="primary" />}
+						label="Remember me"
+					/>
+
+					<Link to="/select-form" ><Button
+						type="submit"
+						fullWidth
+						variant="contained"
+						color="primary"
+					>Connexion</Button></Link>
+
+					<Link href="#" variant="body2">
+						{"Mot de passe oublié ?"}
+					</Link>
+
+				</form>
+				{this.state.success ? <p>Vous êtes connecté.</p> : null}
+			</div>
+		)
+	}
 }
 
 export default Login;
-
