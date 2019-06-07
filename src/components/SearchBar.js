18a5b654a0f2210c917324a4e5e2aa7d22009719
Nocktiss <anthony.grupposo@gmail.com>
2019-06-07 12:15:53 +0200

    add use in login with axios.post

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -7,7 +7,7 @@ class SearchBar extends React.Component {
         return (
             <form role="search" method="get" className='searchForm'>
                 <input className='inputSearch' type="text" placeholder="Recherche..." />
-                <button type="submit" className="boutonLoupe" ><img className="loupe" src="https://image.flaticon.com/icons/png/128/54/54527.png" ></img> </button>
+                <button type="submit" className="boutonLoupe" ><img className="loupe" src="https://image.flaticon.com/icons/png/128/54/54527.png" alt="loupe" ></img> </button>
             </form>
         )
     }
