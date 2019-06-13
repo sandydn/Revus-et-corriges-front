add4327b351c20e32c84da7851a6dfd5d40e6b13
Celian <marot.celian@gmail.com>
2019-06-13 15:02:51 +0200

    search bar fix

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,57 @@
 import React from 'react'
 import './SearchBar.css'
-
+import show from './dataTest.json'
 
 class SearchBar extends React.Component {
+    
+
+        
+    
+state = { input:" " };
+
+handleChange = event => {
+  this.setState({ input: event.target.value });
+};
+
+
+
+
+
+    
     render() {
+        
+        console.log(show)
+
+        let leshow = show.filter(input => show.data === input)
+        console.log(leshow)
+
         return (
-            <form role="search" method="get" className='searchForm'>
-                <input className='inputSearch' type="text" placeholder="Recherche..." />
-                <button type="submit" className="boutonLoupe" ><img className="loupe" src="https://image.flaticon.com/icons/png/128/54/54527.png" ></img> </button>
-            </form>
+            
+
+
+    
+
+
+<div className="searchForm">
+                <form>
+                    <input type="text" id="filter" placeholder="Search for..." ref={input => leshow.map = input}  onChange={this.handleChange} value={ this.state.input}  />
+                </form>
+                
+                
+                
+            </div>
+
+
+
+
+        
+
         )
+        
     }
+
 }
+
+console.log(); 
 
 export default SearchBar;
