d22be915d7c20e1c9e3a45130f58c52f207c1c38
Nocktiss <anthony.grupposo@gmail.com>
2019-06-18 12:35:03 +0200

    Add validator/connexion and redirect for login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,44 +1,45 @@
-import React from 'react'
-import axios from 'axios'
-import { Link } from 'react-router-dom';
+import React from 'react';
+import axios from 'axios';
+import { Link, Redirect } from 'react-router-dom';
+import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
 
-import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 import Checkbox from '@material-ui/core/Checkbox';
-import IconButton from '@material-ui/core/IconButton';
 import FormControl from '@material-ui/core/FormControl';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import Input from '@material-ui/core/Input';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import InputLabel from '@material-ui/core/InputLabel';
 import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import InputAdornment from '@material-ui/core/InputAdornment';
 
-// import iconLogin from '../pictures/login.png'
 import RC from '../pictures/RC.png'
 import './Login.css'
 
 class Login extends React.Component {
   state = {
-    email: "",
-    password: "",
+    formData: {
+      email: '',
+      password: '',
+    },
     showPassword: false,
-    success: false
-  }
+    success: false,
+    redirect: false
+  };
 
+  // Show password or not (icon-eye) //
   toggleShow = () => {
-    this.setState({ showPassword: !this.state.showPassword })
-  }
+    this.setState({ showPassword: !this.state.showPassword });
+  };
 
   handleChange = event => {
-    this.setState({ [event.target.name]: event.target.value })
-    console.log("this state", this.state)
-  }
+    const { formData } = this.state;
+    formData[event.target.name] = event.target.value;
+    this.setState({ formData });
+  };
 
-  handleUploadImage = () => {
-    alert("Your file is being uploaded!")
-  }
-
+  // ON SUBMIT - rediriger avec le routeur sur la page d'appel de formulaire
   handleSubmit = event => {
     event.preventDefault()
     axios.post(`http://localhost:4242/auth/login/`, {
@@ -46,70 +47,85 @@ class Login extends React.Component {
       admin_password: event.target.password.value
     })
       .then(() => {
-        this.setState({ success: true })
-      })
-  }
-  // ON SUBMIT - rediriger avec le routeur sur la page d'appel de formulaire
+        this.setState({ success: true }, () => {
+          setTimeout(() => this.setState({ success: false }), 1400);
+          setTimeout(() => this.setState({ redirect: true }), 1400);  
+        });
+      });
+  };
+
 
   render() {
+    const { formData, success, redirect } = this.state;
+      if (redirect){
+        return <Redirect to='/select-form'/>
+      }
+
     return (
-      <div>
         <form className="adminForm" onSubmit={this.handleSubmit}>
-          {/*<img className="iconLogin" src={iconLogin} alt="icone-login" /> */}
+
           <img className="iconUser" src={RC} alt="icone-user" />
-          <p className="titleLogin">Login</p>
-          <TextField
-            margin="normal"
-            fullWidth
-            id="email"
-            label="Email"
-            name="email"
-            value={this.state.email}
-            onChange={this.handleChange}
-            autoComplete="email"
-            autoFocus
-          />
+          {/* <p className="titleLogin">Login</p> */}
 
-          <FormControl className="">
-            <InputLabel htmlFor="adornment-password">Password</InputLabel>
-            <Input
-              id="password"
-              name="password"
+          <ValidatorForm
+            ref="form"
+            onSubmit={this.handleSubmit}
+          >
+            <TextValidator 
               fullWidth
-              type={this.state.showPassword ? 'text' : 'password'}
-              value={this.state.password}
+              label="Email"
               onChange={this.handleChange}
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton aria-label="Toggle password visibility" onClick={this.toggleShow}>
-                    {this.state.showPassword ? <Visibility /> : <VisibilityOff />}
-                  </IconButton>
-                </InputAdornment>} />
-          </FormControl>
+              name="email"
+              value={formData.email}
+              validators={['required', 'isEmail']}
+              errorMessages={['this field is required', 'Email non valide.']}
+            />
 
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
+            <FormControl>
+              <InputLabel htmlFor="adornment-password">Password</InputLabel>
+              <Input
+                label="Password"
+                name="password"
+                type={this.state.showPassword ? 'text' : 'password'}
+                value={formData.password}
+                onChange={this.handleChange}
+                validators={['required']}
+                errorMessages={['this field is required']}
+                endAdornment={ 
+                  <InputAdornment position="end">
+                    <IconButton aria-label="Toggle password visibility" onClick={this.toggleShow}>
+                      {this.state.showPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>} />
+            </FormControl>
 
-          <Button
-            type="submit"
-            value="Submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-          >Connexion</Button>
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
 
-          {this.state.success ? <p>Vous êtes connecté.</p> : null}
+            <Button
+              type="submit"
+              value="Submit"
+              color="primary"
+              variant="contained"
+              disabled={success}
+              fullWidth
+            >
+              {
+                (success && 'Vous êtes connécté.')
+                || (!success && 'Submit')
+              }
+            </Button>
+          </ValidatorForm>
 
           <Link to="" variant="body2">
             {"Mot de passe oublié ?"}
           </Link>
 
         </form>
-      </div>
-    )
-  }
-}
+    );
+  };
+};
 
 export default Login;
