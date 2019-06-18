3ee47fec6cea6234cf372f08c0ded4e69e095023
Celian <marot.celian@gmail.com>
2019-06-18 14:37:58 +0200

    add glass on search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -34,7 +34,9 @@ handleChange = event => {
 
 <div className="searchForm">
                 <form>
-                    <input type="text" id="filter" placeholder="Search for..." ref={input => leshow.map = input}  onChange={this.handleChange} value={ this.state.input}  />
+                    <input type="text" id="filter" placeholder="Recherche..." ref={input => leshow.map = input}  onChange={this.handleChange} value={ this.state.input}/>
+                    <button  type="submit" className="boutonLoupe" ><img className="loupe" src="https://image.flaticon.com/icons/png/128/54/54527.png" alt='Recherche' ></img> </button>
+
                 </form>
                 
                 
