f11104910fd17996c803812e0e047fb94b48582c
Esoriak <somahaut.dev@gmail.com>
2019-06-19 11:29:57 +0200

    add marge on right for screen admin

--- src/screen/SelectionForm.js
+++ src/screen/SelectionForm.js
@@ -3,11 +3,14 @@ import ButtonForm from '../components/ButtonForm'
 import '../screen/SelectionForm.css'
 
 import { Link } from 'react-router-dom';
+import FormVideo from './FormVideo';
+import FormCine from './FormCine';
+import Formevent from './Formevent';
 
 class SelectionForm extends Component {
   render() {
     return (
-
+<>
         <div className='container-button'>
           <Link to="/formevent" className="choice-button"><ButtonForm name="Evènements" /></Link>
           <Link to="/formcinema" className="choice-button"><ButtonForm name="Cinéma" /></Link>
@@ -15,7 +18,7 @@ class SelectionForm extends Component {
           <Link to="/login" ><input type="submit" value="Retourner à l'accueil" /></Link>
         </div>
        
-
+</>
     )
   }
 }
