555aefa9dc6713c4c66fbf40dc7eedffd67f364a
Esoriak <somahaut.dev@gmail.com>
2019-06-19 14:43:34 +0200

    delete screen form and replace by screen for admin - managment events videos and cinema

---
+++ src/screen/Admin.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import SelectionForm from './SelectionForm';
+
+class Admin extends Component {
+    render() {
+        return (
+            <div>
+                <SelectionForm />
+            </div>
+        )
+    }
+}
+
+export default Admin
