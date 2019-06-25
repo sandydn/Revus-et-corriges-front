6450f8754d284e81f93349deba9c7fc2dbeeeffa
marc <lalliermarc8888@yahoo.fr>
2019-06-25 17:19:43 +0200

    a color picker has been added in Admin.js, waiting for update

---
+++ src/components/Parameter.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react'
+
+import "./Parameter.css"
+import { SketchPicker } from 'react-color';
+
+
+
+export class Parameter extends Component {
+
+  state = {
+    show:true,
+    color:""
+  }
+ 
+
+  handleShow = () => {
+    this.setState({ show : !this.state.show });
+  }
+
+
+   handleChange = (color) => {
+   this.setState({ color: color.hex});
+  }
+
+
+  render() {
+
+    const effect = this.state.show ? "showing" : "notShowing"
+    console.log(this.state)
+    
+    return (
+      
+      <div>
+        
+        <h1>BOUTON PARAMETRE</h1>
+
+        <div>
+          <img 
+            className = "wheelButton"
+            onClick={this.handleShow} 
+            src= 'https://pngimage.net/wp-content/uploads/2018/06/logo-engrenage-png-5.png' 
+          /> 
+        </div>
+
+        <div>
+          <SketchPicker 
+            className = {effect}  
+            color={ this.state.color }
+            onChangeComplete={ this.handleChange }/>
+        </div> 
+
+      </div>
+    )
+  }
+}
+export default Parameter
+
+
+
+
+
+
+
+
+
+
+
+
--- src/screen/Admin.js
+++ src/screen/Admin.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import SelectionForm from './SelectionForm';
+import Parameter from '../components/Parameter';
 
 class Admin extends Component {
     render() {
         return (
             <div>
                 <SelectionForm />
+                <Parameter />
             </div>
         )
     }
