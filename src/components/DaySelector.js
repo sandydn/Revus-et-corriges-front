b151a752c0422bc528c83c72dd8158d47082094b
QuietMonkey <taillandierbertrand@gmail.com>
2019-06-26 12:52:06 +0200

    Create Logic for MobileDisplay

---
+++ src/components/DaySelector.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import MobileDays from './MobileDays'
+
+const DaySelector = ({dataDays}) => {
+    return(
+        dataDays.map((day)=> <MobileDays date={day.date}/>)
+    )
+}
+
+export default DaySelector
