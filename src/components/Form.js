84bd353ebc49c022ddbe104f970d481bec8a9471
Esoriak <somahaut.dev@gmail.com>
2019-07-02 16:26:13 +0200

    update for props on input and form

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import InputInLine from './InputInLine';
 import Checkboxline from './CheckboxLine';
 import './Form.css';
+import Dropdown from './Dropdown';
 
 class Form extends Component {
     state = {
@@ -12,10 +13,20 @@ class Form extends Component {
         date: null,
         lien: null,
         information: null,
+        realisateur: null,
+        datefilm: null,
+        distributeur: null,
+        format: null,
+        editeur: null,
+        visuel: null,
     }
 
     handleChangeInput = (keyState, evt) => {
         this.setState({ [keyState] : evt.target.value })
+    }
+
+    handleChangeDropdown = (keyState, evt) => {
+        this.setState({ [keyState] : evt.target.value})
     }
     
     render() {
@@ -30,7 +41,7 @@ class Form extends Component {
         } = this.state
         console.log(this.state)
         return (
-            <div>
+            <div className="Form">
                 <InputInLine
                 keyState="date1"
                 title="Date de début"
@@ -43,12 +54,12 @@ class Form extends Component {
                 value={date2}
                 funct={this.handleChangeInput}
             />
-            <InputInLine
+            {/* <InputInLine
                 keyState="titre"
                 title="Titre"
                 value={titre}
                 funct={this.handleChangeInput}
-            />
+            /> */}
             <InputInLine
                 keyState="lieu"
                 title="Lieu"
@@ -73,11 +84,55 @@ class Form extends Component {
                 value={information}
                 funct={this.handleChangeInput}
             />
+            <InputInLine
+                keyState="realisateur"
+                title="réalisateur"
+                value={realisateur}
+                funct={this.handleChangeInput}
+            />
+            <InputInLine
+                keyState="datefilm"
+                title="Date du film"
+                value={datefilm}
+                funct={this.handleChangeInput}
+            />
+            <InputInLine
+                keyState="distributeur"
+                title="Distributeur"
+                value={distributeur}
+                funct={this.handleChangeInput}
+            />
+            <InputInLine
+                keyState="format"
+                title="format"
+                value={format}
+                funct={this.handleChangeInput}
+            />
+            <InputInLine
+                keyState="editeur"
+                title="éditeur"
+                value={editeur}
+                funct={this.handleChangeInput}
+            />
+            <InputInLine    
+                keyState="visuel"
+                title="visuel"
+                value={visuel}
+                funct={this.handleChangeInput}
+            />
+
             <div className="checkbox">
             <Checkboxline title="r&c"/>
             <Checkboxline title="partenaires" />
             <Checkboxline title="général"/>
             </div>
+
+            <Dropdown 
+                keyState="titre"
+                title="titre du film" 
+                value={titre}
+                funct={this.handleChangeDropdown}
+            />
         </div>
         )
     }
