75f7157510a40a2e434f93dd4ca226ed34812aef
marc <lalliermarc8888@yahoo.fr>
2019-07-02 11:31:33 +0200

    add a dropdown without logic in Form.js, waiting a meeting to discuss the way how to connect every elements

---
+++ src/components/Dropdown.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class Dropdown extends Component {
+  render() {
+    return (
+      <div>
+        <select>
+          <option value="volvo">C.Lyan</option>
+          <option value="saab">Sandy</option>
+          <option value="opel">Kiran</option>
+          <option value="audi">Solene</option>
+          <option value="audi">Bertrand</option>
+          <option value="Chaudar">Chaudar</option>
+</select>
+      </div>
+    )
+  }
+}
+
+export default Dropdown
