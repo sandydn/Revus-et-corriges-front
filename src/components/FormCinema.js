3db4be5e412375be6313f04d41737d0e94759d65
Esoriak <somahaut.dev@gmail.com>
2019-07-10 14:08:09 +0200

    add axios on form movie - without dropdown

--- src/components/FormCinema.js
+++ src/components/FormCinema.js
@@ -1,24 +1,19 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
-
-import CheckboxLine from './CheckboxLine';
 import InputInLine from './InputInLine';
+import CheckboxLine from './CheckboxLine';
 import InputWithCalendar from './InputWithCalendar'
 import MenuAdmin from '../screen/MenuAdmin';
-
+import axios from 'axios';
 import './Form.css';
 
 class FormCinema extends Component {
   state = {
-    dateCreation: null,
+    description: null,
+    category: null,
     dateStart: null,
-    // Distributeur: [nom, prenom, type],
-    nom: [],
-    prenom: [],
-    type: [],
-    importance: null,
+    importance: [],
     link: null,
-    // Realisateur: [nom, prenom, type ],
     titre: null,
     cover: null,
   }
@@ -57,64 +52,63 @@ class FormCinema extends Component {
   }
 
 
-  // // ON SUBMIT - envoyer les informations de l'evenement dans la bdd
-  // handleSubmit = (e) => {
-  //   e.preventDefault()
-  //   console.log(e)
-  //   axios.post(`http://localhost:4000/a5/event`, {
-  //     // dateStart: e.target.dateStart.value,
-  //     // dateEnd: e.target.dateEnd.value,
-  //     importance: e.target.importance.value,
-  //     description: e.target.description.value,
-  //     link: e.target.link.value,
-  //     cover: e.target.cover.value,
-  //     titre: e.target.titre.value,
-  //     lieux_idlieux: e.target.adresse.value,
+  // ON SUBMIT - envoyer les informations de l'evenement dans la bdd
+  handleSubmit = (e) => {
+    e.preventDefault()
+    axios.post(`http://localhost:4000/a5/event`, {
+      // dateStart: e.target.dateStart.value,
+      importance: this.state.importance,
+      category: this.state.category,
+      description: this.state.description,
+      link: this.state.link,
+      cover: this.state.cover,
+      // titre: this.state.titre,
 
-  //   })
-  // }
+
+    })
+  }
 
   render() {
     const {
-      dateCreation,
+      description,
       dateStart,
-      // Distributeur: [nom, prenom, type],
-      nom,
-      prenom,
-      type,
-      // général,
       link,
-      // partenaires,
-      // rc,
-      // Realisateur: [nom, prenom, type ],
-      titre,
       cover,
+      category,
     } = this.state
 
     return (
       <div className="screen">
         <MenuAdmin />
         <form className="Formcinema" onSubmit={this.handleSubmit}>
-          <p>Date de debut :</p>
+          {/* <p>Date de debut :</p>
           <InputWithCalendar
             date={dateStart}
             onChangeDate={this.onChangeDateStart}
-          />
-
-TODO dropdown importance
-
-TODO dropdown titre de film
-          {/* <InputInLine
-            keyState="titre"
-            title="Titre"
-            value={titre}
-            funct={this.handleChangeInput}
           /> */}
 
+          TODO dropdown importance
+          
+          TODO dropdown titre de film
+          
+            <div className="importance">
+            <p>Importance </p>
+            <CheckboxLine title="r&c" keyState="importance" value={1} funct={this.handleChangeInput} />
+            <CheckboxLine title="partenaires" keyState="importance" value={2} funct={this.handleChangeInput} />
+            <CheckboxLine title="général" keyState="importance" value={3} funct={this.handleChangeInput} />
+          </div>
+
           <InputInLine
-            keyState="dateCreation"
-            title="Date du film"
-            value={dateCreation}
+            keyState="category"
+            title="catégorie"
+            value={category}
+            funct={this.handleChangeInput}
+          />
+
+          <InputInLine
+            keyState="description"
+            title="information"
+            value={description}
             funct={this.handleChangeInput}
           />
 
@@ -132,16 +126,16 @@ TODO dropdown titre de film
             funct={this.handleChangeInput}
           />
 
-            <button
-              className="button-submit"
-              type="submit"
-              value="Submit"
-              color="primary"
-              variant="contained"
-            >Envoyer le formulaire
+          <button
+            className="button-submit"
+            type="submit"
+            value="Submit"
+            color="primary"
+            variant="contained"
+          >Envoyer le formulaire
             </button>
-          </form>
-        </div>
+        </form>
+      </div>
     )
   }
 }
