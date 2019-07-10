e7e2af6be94906ed6a711e12f4f779257e44e31b
Nocktiss <anthony.grupposo@gmail.com>
2019-07-10 17:26:53 +0200

    No changement

--- src/components/RegisterLogin.js
+++ src/components/RegisterLogin.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import { NavLink, Redirect } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
 
 import Button from '@material-ui/core/Button';
