692913035bc34e970837c949c513b29513b8d886
Sandy Das Neves <sandydn78@gmail.com>
2019-07-11 02:09:09 +0200

    add delete fonctions

---
+++ src/components/DeleteForm.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+import Button from '@material-ui/core/Button';
+// import InputInLine from './InputInLine';
+
+class DeleteForm extends Component {
+
+    state = {
+        idevent: null,
+        titre: null,
+        importance: null,
+        description: null,
+        dateStart: null,
+        dateEnd: null,
+        cover: null,
+        link: null,
+        adresse: null,
+        category: null
+    }
+
+    handleChange = (evt) => {
+        console.log("evt", evt.target.value)
+        this.setState({ [evt.target.name]: evt.target.value })
+    }
+
+    classDelete = this.props.data === null ? 'deleteNone' : 'delete'
+
+    render() {
+        console.log(this.props.data)
+        return (
+
+        <div className={this.classDelete}>
+            <div>{this.props}</div>
+            <input
+                title="Modifier" 
+                name="titre"   
+                value={this.state.name}
+                placeholder={this.props.data}
+                onChange={this.handleChange}
+            />
+            <Button
+                className="button-submit"
+                type="submit"
+                value="Submit"
+                color="grey"
+                variant="contained"
+            >
+                Valider
+            </Button>
+        </div>
+
+        )
+    }
+}
+
+export default DeleteForm
