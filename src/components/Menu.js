02d3f5f003888c0651ad5beae1c3fca324a39d2b
Celian <marot.celian@gmail.com>
2019-07-16 15:11:32 +0200

    delete useless class name

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -20,7 +20,7 @@ render() {
                 <li className='liDesckop'><a className='aLinkRC' href="https://revusetcorriges.net/boutique/sabonner/" >BOUTIQUE </a></li>      
                 <li className='liDesckop'><a className='aLinkRC' href="https://revusetcorriges.net/qui-sommes-nous/" >QUI SOMMES-NOUS ? </a></li>
                 <li className='liDesckop'><a className='aLinkRC' href="https://revusetcorriges.net/revue-en-ligne/" >LA REVUE EN LIGNE </a></li>
-                <li className='liDesckop'><NavLink className='aLinkRC' exact to="/" activeClassName="selected">CALENDRIER</NavLink> </li>
+                <li className='liDesckop'><NavLink className='aLinkRC' exact to="/" >CALENDRIER</NavLink> </li>
                 <li className='liDesckop'><a className='aLinkRC' href="https://revusetcorriges.net/contact/nous-contacter/" >CONTACT+ </a></li>
                 <SearchBar/>
             </ul>
