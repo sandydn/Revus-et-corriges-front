ca669a39b4f8d202c6f4a27f3675acf3b756b415
Esoriak <somahaut.dev@gmail.com>
2019-07-16 17:50:18 +0200

    add axios for post colorpicker for textcolor and fix css un screen Ajout

--- src/components/Parameter.js
+++ src/components/Parameter.js
@@ -2,62 +2,88 @@ import React, { Component } from 'react'
 
 import "./Parameter.css"
 import { SketchPicker } from 'react-color';
-import "react-datepicker/dist/react-datepicker.css"
- class Parameter extends Component {
+import axios from 'axios'
+
+
+class Parameter extends Component {
 
   state = {
-    show:true,
-    color:"",
+    show: false,
+    color: "",
+    background: "",
+    textfont: ""
   }
- 
+
 
   handleShow = () => {
-    this.setState({ show : !this.state.show });
+    this.setState({ show: !this.state.show });
   }
 
 
-   handleChange = (color) => {
-   this.setState({ color: color.hex});
+  handleChange = (color) => {
+    this.setState({ color: color.hex });
   }
 
   onChange = () => {
     this.setState({ color: '' });
- }
+  }
 
-
+  handleSubmit = (e) => {
+    e.preventDefault()
+    axios.post(`http://localhost:4000/a4/decoration`, {
+      textcolor: this.state.color,
+      background: this.state.background,
+      textfont: this.state.textfont
+    })
+  }
 
   render() {
+    console.log(this.state.color);
+    
 
     const effect = this.state.show ? "showing" : "notShowing"
-    console.log(this.state)
-    
+
     return (
-      
-      <div  
-        style={{ backgroundColor: this.state.color }} 
-        onClick={this.onChange}
-      >
-        
-        <h1>BOUTON PARAMETRE</h1>
 
-        <div>
-          <img 
-            className = "wheelButton"
-            onClick={this.handleShow} 
-            src= 'https://pngimage.net/wp-content/uploads/2018/06/logo-engrenage-png-5.png' 
-          /> 
-        </div>
+      <>
+        <h3>Changer la couleur de police du calendrier</h3>
+        <form className="updatefontcolor" onSubmit={this.handleSubmit}>
+          {/* <div> */}
+            <img
+              className="wheelButton"
+              onClick={this.handleShow}
+              src='https://pngimage.net/wp-content/uploads/2018/06/logo-engrenage-png-5.png'
+            />
+          {/* </div> */}
+          <div
+            style={{ backgroundColor: this.state.color }}
+            onClick={this.onChange}
+            className="view-color"
+          >
+            {/* <div> */}
+              {/* <div> */}
+                <SketchPicker
+                  className={effect}
+                  color={this.state.color}
+                  onChangeComplete={this.handleChange}
+                />
+                <input
+                  name="Valeur hexadécimale"
+                  value={this.state.color}
+                />
+                <input onClick={this.handleSubmit}
+                  className="button-submit"
+                  type="submit"
+                  value="Envoyer"
+                  color="grey"
+                  variant="contained"
+                />
+              </div>
+            {/* </div>
+          </div> */}
+        </form>
+      </>
 
-        <div>
-          <SketchPicker 
-            className = {effect}  
-            color={ this.state.color }
-            onChangeComplete={ this.handleChange }/>
-        </div> 
-
-
-
-      </div>
     )
   }
 }
